fd8930213c422de14b41be03f6c159073a3df15a
Rikelmelopes <rikelme.lopes777@gmail.com>
2023-02-01 18:43:13 -0300

    Começando

---
+++ ex1.py
@@ -0,0 +1,13 @@
+nome = 'Rikelme'
+sobrenome = 'Lopes'
+idade = 22
+ano_nascimento = 2023 - idade
+maior_de_idade = idade >= 18
+altura_em_metros = 1.70
+
+print('Nome:',nome)
+print('Sobrenome:',sobrenome)
+print('Idade:',idade,'anos')
+print('Ano do nascimento:',ano_nascimento)
+print('È maior de idade?',maior_de_idade)
+print('Altura em metros:',altura_em_metros,)
