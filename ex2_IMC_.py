831239f75222a73db2b6a5ca063600140239b6c4
Rikelmelopes <rikelme.lopes777@gmail.com>
2023-02-01 19:01:17 -0300

    exexcicio do IMC

---
+++ ex2_IMC_.py
@@ -0,0 +1,8 @@
+#Calcule o IMC
+nome = 'Rikelme'
+altura = 1.70
+peso = 50
+
+imc = peso /(altura * altura)
+
+print('{} tem {} de altura e pesa {} ,seu IMC é de {:2f}'.format(nome,altura,peso ,imc))
