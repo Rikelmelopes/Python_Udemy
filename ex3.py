ca4261a304ad8a062ca8ef8eb1532ca93b5eabc4
Rikelmelopes <rikelme.lopes777@gmail.com>
2023-02-06 19:26:32 -0300

    Exercicio 3

--- ex3.py
+++ ex3.py
@@ -2,9 +2,11 @@ n1 = int(input('Digite o primeiro valor:  '))
 n2 = int(input('Digite o segundo valor:  '))
 
 if n1 > n2:
-    print('O primeiro valor é maior do que o segundo valor')
+    print('{} é maior do que {}'.format(n1,n2))
+elif n1 == n2:
+    print('{} e {} são iguais'.format(n1,n2))
 else:
-    print('O segundo valor é maior do que o primeiro valor')
+    print('{} é maior do que {}'.format(n2,n1))
 
 
 
