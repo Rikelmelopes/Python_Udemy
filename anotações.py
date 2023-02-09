8af59b502b0c1a3242ab10499c1987889b86d68f
Rikelmelopes <rikelme.lopes777@gmail.com>
2023-02-09 18:17:32 -0300

    Exercicio 4

--- anotações.py
+++ anotações.py
@@ -1,1 +1,12 @@
-print('Explícito', 'é', 'melhor " do que implícito')
+numero = 10
+
+if numero > 1:
+    if numero > 2:
+        if numero > 3:
+            print('Número maior que 3')
+        else:
+            print('Número menor que 3')
+    else:
+        print('Número menor que 2')
+else:
+    print('Número menor que 1')
